8f4070d95c7a62895aff25fab6c5dbc91e022d02
Mac <mac@Macs-iMac-2.local>
2018-08-07 10:05:00 +0545

    json-server setup

---
+++ db.js
@@ -0,0 +1,21 @@
+module.exports = () => {
+  return {
+    superHeroes: [
+      {
+        id: 1,
+        name: "Iron Man",
+        realName: "Tony Stark",
+        superPowers:
+          "Armor suit with superhuman strength, supersonic flight and missile projection",
+        avatar: "tony-stark.jpg"
+      },
+      {
+        id: 2,
+        name: "Thor",
+        realName: "Tony Thor Odinson",
+        superPowers: "Super strenght, control lightening, possess stormbreaker",
+        avatar: "thor-odinson.jpg"
+      }
+    ]
+  };
+};
